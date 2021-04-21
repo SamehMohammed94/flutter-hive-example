17109e35282dd3e7cec624b5c0f51717dc5f6606
Sameh Mohammed <samehmohammeddarrag1994@gmail.com>
2021-04-21 23:27:24 +0200

    ..

--- lib/views/home-view.dart
+++ lib/views/home-view.dart
@@ -13,7 +13,7 @@ class HomeView extends StatelessWidget {
           "Hive (Family App)",
           style: TextStyle(color: Colors.black87),
         ),
-        centerTitle: true, // for cernter Master
+        centerTitle: true, // Master
       ),
       body: FamilyListView(),
       floatingActionButton: FloatingActionButton.extended(
